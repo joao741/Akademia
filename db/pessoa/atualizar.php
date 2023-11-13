0e1f7aea2770e37d246210780405f19d03351f95
João Victor <victorjoaothomaz@gmail.com>
2023-11-13 11:19:03 -0300

    update

---
+++ db/pessoa/atualizar.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "ti47";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $sql = "UPDATE cadastro SET telefone='999' WHERE idcadastro=2";
+
+  // Prepare statement
+  $stmt = $conn->prepare($sql);
+
+  // execute the query
+  $stmt->execute();
+
+  // echo a message to say the UPDATE succeeded
+  echo $stmt->rowCount() . " records UPDATED successfully";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
